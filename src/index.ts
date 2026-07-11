1cb24d2630df4ba498a90480b21524db96a33d7e
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-11 13:14:38 +0200

    v1.0.115

--- src/index.ts
+++ src/index.ts
@@ -96,7 +96,6 @@ export function dpuseBaseESLintConfig(options: DPUseBaseESLintConfigOptions): Li
                     }
                 ],
                 'unicorn/filename-case': ['error', { cases: { camelCase: true, pascalCase: true } }],
-                'no-non-function-verb-prefix': 'off',
                 'unicorn/no-null': 'off', // Null is required for JSON interop.
                 'unicorn/switch-case-braces': ['warn', 'avoid'],
 
